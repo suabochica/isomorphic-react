c7ced69f8596f10e035d669af90717cfc76a588b
Sergio Benítez <sergio.benitez@getbuilt.com>
2022-03-10 09:38:39 -0500

    jest: understanding jest globals

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,6 +1,26 @@
 
 describe(`The Question List Component`, () => {
-    it (`should display a list of questions`, () => {
+    beforeEach(() => {
+        console.log("Before each!")
+    });
 
+    beforeAll(() => {
+        console.log("Before all!")
+    });
+
+    afterEach(() => {
+        console.log("After each!")
+    });
+
+    afterAll(() => {
+        console.log("After all!")
+    });
+
+    it.only(`should display a list of questions`, () => {
+        expect(40 + 2).toEqual(42);
+    });
+
+    it(`should display a list of questions`, () => {
+        expect(40 + 2).toEqual(43);
     });
 });
