e2b21326473c69c0dc1fa97e219bc786c2766851
Sergio Benítez <sergio.benitez@getbuilt.com>
2022-03-14 09:11:08 -0500

    jest: mock isomorphic-fetch package

--- src/sagas/fetch-question-saga.js
+++ src/sagas/fetch-question-saga.js
@@ -5,7 +5,7 @@ export default function* () {
     yield takeEvery('REQUEST_FETCH_QUESTION', handleFetchQuestion);
 }
 
-function* handleFetchQuestion({ question_id }) {
+export function* handleFetchQuestion({ question_id }) {
     const raw = yield fetch(`/api/questions/${question_id}`);
     const json = yield raw.json();
     const question = json.items[0];
